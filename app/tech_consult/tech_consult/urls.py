385ff112966102941bf9ae5094542b01a0c96f22
Marissa Lee <marissa@d-172-25-108-186.dhcp.virginia.edu>
2018-02-14 16:26:14 -0500

    Fixed models.py and started simple view for index and GET

--- app/tech_consult/tech_consult/urls.py
+++ app/tech_consult/tech_consult/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from webapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^api/v1/consumers/(?P<consumer_pk>[0-9]+)/$', views.get_consumer, name='get_consumer'),
 ]
