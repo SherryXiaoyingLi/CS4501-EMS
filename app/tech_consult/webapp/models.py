3c4d3c1aa273d128194fab441a4d05a70f47055c
Marissa Lee <marissa@d-172-25-108-186.dhcp.virginia.edu>
2018-02-14 16:26:14 -0500

    Fixed models.py and started simple view for index and GET

--- app/tech_consult/webapp/models.py
+++ app/tech_consult/webapp/models.py
@@ -1,8 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class ConsumerRequest(models.model):
+class Consumer(models.Model):
+    username = models.CharField(max_length=200, default="none", unique=True)
+    password = models.CharField(max_length=200, default="none")
+    first_name = models.CharField(max_length=200, default="none")
+    last_name = models.CharField(max_length=200, default="none")
+    phone = models.CharField(max_length=200, default="none")
+    email = models.EmailField(max_length=200, default="none")
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return "%s" % (self.username)
+
+class Review(models.Model):
+    rating = models.IntegerField(default = 0)
+    author = models.CharField(max_length=50, default="none")
+    comment = models.CharField(max_length=150, default="none")
+
+class Producer(models.Model):
+    username = models.CharField(max_length=200, default="none", unique=True)
+    password = models.CharField(max_length=200, default="none")
+    first_name = models.CharField(max_length=200, default="none")
+    last_name = models.CharField(max_length=200, default="none")
+    phone = models.CharField(max_length=200, default="none")
+    email = models.EmailField(max_length=200, default="none")
+    bio = models.CharField(max_length=250, default="none")
+    skills = models.CharField(max_length=200, default="none")
+    review = models.ForeignKey(Review)
+    #consumer = models.ForeignKey(Consumer)
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return "%s" % (self.username)
+
+class ConsumerRequest(models.Model):
     title = models.CharField(max_length=50, default="none")
     offered_price = models.FloatField(max_length=10)
     description = models.CharField(max_length=200)
@@ -13,36 +47,3 @@ class ConsumerRequest(models.model):
 
     def __str__(self):
         return self.description
-
-class Consumer(models.model):
-    username = models.CharField(max_length=200, default="none", unique=True)
-	password = models.CharField(max_length=200, default="none")
-	first_name = models.CharField(max_length=200, default="none")
-	last_name = models.CharField(max_length=200, default="none")
-	phone = models.CharField(max_length=200, default="none")
-    email = models.EmailField(max_length=200, default="none")
-    user = models.ForeignKey(User)
-
-    def __str__(self):
-        return "%s" % (self.username)
-
-class Producer(models.model):
-    username = models.CharField(max_length=200, default="none", unique=True)
-	password = models.CharField(max_length=200, default="none")
-	first_name = models.CharField(max_length=200, default="none")
-	last_name = models.CharField(max_length=200, default="none")
-	phone = models.CharField(max_length=200, default="none")
-    email = models.EmailField(max_length=200, default="none")
-    bio = models.CharField(max_length=250, default="none")
-    skills = models.CharField(max_length=200, default="none")
-    review = models.ForeignKey(Review)
-    consumer = models.ForeignKey(Consumer)
-    user = models.ForeignKey(User)
-
-    def __str__(self):
-        return "%s" % (self.username)
-
-class Review(models.model):
-    rating = models.IntegerField(default = 0)
-    author = models.CharField(max_length=50, default"none")
-    comment = models.CharField(max_length=150, default"none")
--- app/tech_consult/webapp/views.py
+++ app/tech_consult/webapp/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+
+import json
+from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import Consumer, ConsumerRequest, Producer, Review
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("CS4501 Project 2")
+
+def get_consumer (request, consumer_pk):
+    #consumer = Consumer.objects.get(pk=consumer_pk)
+    response_data = {}
+    if request.method == 'GET':
+
+        #Uncommented until we load database with data
+        #response_data['pk'] = consumer.pk
+        #response_data['username'] = consumer.username
+        #response_data['password'] = consumer.password
+        #response_data['first_name'] = consumer.first_name
+        #response_data['last_name'] = consumer.last_name
+        #response_data['phone'] = consumer.phone
+        #response_data['email'] = consumer.email
+
+        response_data['pk'] = 1
+        response_data['username'] = 'myl2vu'
+        response_data['password'] = 'ilikeseals'
+        response_data['first_name'] = 'marissa'
+        response_data['last_name'] = 'lee'
+        response_data['phone'] = '7037316926'
+        response_data['email'] = 'myl2vu@virginia.edu'
+
+        #return JsonResponse({'test': 'value'})
+        return JsonResponse(response_data)
+    elif request.method == 'POST':
+
+        # consumer.save()
+
+        # Uncommented until we load database with data
+        # response_data['pk'] = consumer.pk
+        # response_data['username'] = consumer.username
+        # response_data['password'] = consumer.password
+        # response_data['first_name'] = consumer.first_name
+        # response_data['last_name'] = consumer.last_name
+        # response_data['phone'] = consumer.phone
+        # response_data['email'] = consumer.email
+
+        return JsonResponse(response_data)
+    return HttpResponse("Needs a GET or POST operation")
