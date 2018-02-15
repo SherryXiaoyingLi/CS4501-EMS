7a9d1288dafd10326e37e2433c0e526cf1c45928
LiXiaoying <lixiaoying@d-172-25-99-176.dhcp.virginia.edu>
2018-02-15 17:31:22 -0500

    All services but need to drop db tables

--- app/tech_consult/webapp/forms.py
+++ app/tech_consult/webapp/forms.py
@@ -26,7 +26,19 @@ class UpdateProducerForm(forms.Form):
     bio = forms.CharField(max_length=250, help_text="Bio: ")
     skills = forms.CharField(max_length=200, help_text="Skills: ")
 
-    class Meta:
-        # Provide an association between the ModelForm and a model
-        model = Producer
-        fields = ('username', 'password', 'first_name', 'last_name', 'phone', 'email', 'bio', 'skills')
+
+class UpdateReviewForm(forms.Form):
+    rating = forms.IntegerField(help_text="Rating: ")
+    comment = forms.CharField(max_length=150, help_text="Comment: ")
+    author = forms.IntegerField(help_text="Author PK: ")
+    producer =forms.IntegerField(help_text="Producer PK: ")
+
+
+class UpdateConsumerRequestForm(ModelForm):
+    title = forms.CharField(max_length=200, help_text="Title: ")
+    offered_price = forms.FloatField(help_text="Offered Price: ")
+    description = forms.CharField(max_length=200, help_text="Description: ")
+    timestamp = forms.CharField(max_length=200, help_text="Timestamp: ")
+    availability = forms.CharField(max_length=15, help_text="Availability: ")
+    consumer = forms.IntegerField(help_text="Consumer PK: ")
+    accepted_producer = forms.IntegerField(help_text="Producer PK: ")
