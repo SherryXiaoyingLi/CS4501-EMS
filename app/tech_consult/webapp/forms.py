3ec413087eebc5acde7ede0277d4a031ffff470c
Elliott Kim <ek4tx@virginia.edu>
2018-02-15 00:03:17 -0500

    producer views

--- app/tech_consult/webapp/forms.py
+++ app/tech_consult/webapp/forms.py
@@ -15,3 +15,18 @@ class UpdateConsumerForm(forms.Form):
         # Provide an association between the ModelForm and a model
         model = Consumer
         fields = ('username', 'password', 'first_name', 'last_name', 'phone', 'email')
+
+class UpdateProducerForm(forms.Form):
+    username = forms.CharField(max_length=200, help_text="Username: ")
+    password = forms.CharField(max_length=200, help_text="Password: ")
+    first_name = forms.CharField(max_length=200, help_text="First Name: ")
+    last_name = forms.CharField(max_length=200, help_text="Last Name: ")
+    phone = forms.CharField(max_length=15, help_text="Phone Number: ")
+    email = forms.CharField(max_length=200, help_text="Email Address: ")
+    bio = forms.CharField(max_length=250, help_text="Bio: ")
+    skills = forms.CharField(max_length=200, help_text="Skills: ")
+
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Producer
+        fields = ('username', 'password', 'first_name', 'last_name', 'phone', 'email', 'bio', 'skills')
