ce879278fc27cb583047b20ec7e88a820fc52f90
Shardi3 <myl2vu@virginia.edu>
2018-02-27 20:59:48 -0500

    Fixed consumerrequest bug for get

--- app/tech_consult/webapp/views.py
+++ app/tech_consult/webapp/views.py
@@ -539,7 +539,10 @@ def get_consumerRequest(request, consumerRequest_pk):
             response_data['timestamp'] = consumerRequest.timestamp
             response_data['availability'] = consumerRequest.availability
             response_data['consumer'] = consumerRequest.consumer.pk
-            response_data['accepted_producer'] = consumerRequest.producer.pk
+            if consumerRequest.accepted_producer != None:
+                response_data['accepted_producer'] = consumerRequest.accepted_producer.pk
+            else:
+                response_data['accepted_producer'] = None
 
 
         except:
