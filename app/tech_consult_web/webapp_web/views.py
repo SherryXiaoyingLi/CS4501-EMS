07680d9e2a5a966e507394d75dc718d97e506dcf
Marissa Lee <marissa@Marissas-MacBook-Pro.local>
2018-03-10 19:04:31 -0500

    Fixed web page

--- app/tech_consult_web/webapp_web/views.py
+++ app/tech_consult_web/webapp_web/views.py
@@ -10,9 +10,9 @@ import urllib.parse
 
 # Create your views here.
 def index(request):
-    context_dict = {‘newest_pk’: 1, ‘highest_pk’: 1}
+    context_dict = {'newest_pk': 1, 'highest_pk': 1}
     return render(request, "index.html", context_dict)
 
 def request_detail(request, consumerRequest_pk):
-    context_dict = {‘title’: ‘New to AWS, ‘description’: ‘Looking for someone to teach me AWS’, ‘offered_price’: 75.0, ‘timestamp’: ‘March 9, 2018.  7:55’, ‘availability’: ‘Mondays and Wednesdays’, ‘consumer_username’: ‘bob1’, ‘consumer_email’: ‘bob1@gmail.com’, ‘consumer_phone’: ‘434-958-2913’, ‘producer_username’: null}
+    context_dict = {'title': 'New to AWS', 'description': 'Looking for someone to teach me AWS', 'offered_price': 75.0, 'timestamp': 'March 9, 2018.  7:55', 'availability': 'Mondays and Wednesdays', 'consumer_username': 'bob1', 'consumer_email': 'bob1@gmail.com', 'consumer_phone': '434-958-2913', 'producer_username': None}
     return render(request, "request_detail.html", context_dict)
