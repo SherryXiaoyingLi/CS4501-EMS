ce5d3f479386e1f5457a1df9c9e3f15ebc9cbbc6
Elliott Kim <ek4tx@virginia.edu>
2018-03-12 10:43:23 -0400

    Experience and Web layer views

--- app/tech_consult_exp/webapp_exp/views.py
+++ app/tech_consult_exp/webapp_exp/views.py
@@ -17,16 +17,79 @@ def index(request):
 # Returns the pk of the newest consumer request
 def getNewestRequestPk(request):
     response = {}
+    response_data = {}
+    if request.method == 'GET':
+        try:
+            req = urllib.request.Request('http://models-api:8000/api/v1/consumerRequests/getNewest')
+            resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+            results = json.loads(resp_json)
+            newest_pk = results["result"]["pk"]
+            response["ok"] = True
+            response_data["pk"] = newest_pk
+        except:
+            response["ok"] = False
+
+        response['result'] = response_data
     return JsonResponse(response)
 
 # For index page
 # Returns the pk of the consumer request with the highest price
 def getHighestRequestPk(request):
     response = {}
+    response_data = {}
+    if request.method == 'GET':
+        try:
+            req = urllib.request.Request('http://models-api:8000/api/v1/consumerRequests/getHighestPrice')
+            resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+            results = json.loads(resp_json)
+            newest_pk = results["result"]["pk"]
+            response["ok"] = True
+            response_data["pk"] = newest_pk
+        except:
+            response["ok"] = False
+
+        response['result'] = response_data
     return JsonResponse(response)
 
 # For request detail page
 # Returns the request information, consumer information, and producer username
 def getRequestDetail(request, consumerRequest_pk):
     response = {}
+    response_data = {}
+
+    req = urllib.request.Request('http://models-api:8000/api/v1/consumerRequests/' + str(consumerRequest_pk))
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    results = json.loads(resp_json)
+
+    if results['ok'] == True:
+        consumer_pk = results['result']['consumer']
+        req2 = urllib.request.Request('http://models-api:8000/api/v1/consumers/' + str(consumer_pk))
+        resp_json2 = urllib.request.urlopen(req2).read().decode('utf-8')
+        results2 = json.loads(resp_json2)
+        # consumer_username = results2['result']['username']
+
+        response['ok'] = True
+
+        response_data['title'] = results['result']['title']
+        response_data['description'] = results['result']['description']
+        response_data['offered_price'] = results['result']['offered_price']
+        response_data['timestamp'] = results['result']['timestamp']
+        response_data['availability'] = results['result']['availability']
+        response_data['consumer_username'] = results2['result']['username']
+        response_data['consumer_email'] = results2['result']['email']
+        response_data['consumer_phone'] = results2['result']['phone']
+
+        accepted_producer = results['result']['accepted_producer']
+        if accepted_producer != None:
+            req3 = urllib.request.Request('http://models-api:8000/api/v1/producers/' + str(accepted_producer))
+            resp_json3 = urllib.request.urlopen(req3).read().decode('utf-8')
+            results3 = json.loads(resp_json3)
+            response_data['producer_username'] = results3['result']['username']
+        else:
+            response_data['producer_username'] = None
+
+    else:
+        response['ok'] = False
+
+    response['result'] = response_data
     return JsonResponse(response)
--- app/tech_consult_web/webapp_web/views.py
+++ app/tech_consult_web/webapp_web/views.py
@@ -11,8 +11,49 @@ import urllib.parse
 # Create your views here.
 def index(request):
     context_dict = {'newest_pk': 1, 'highest_pk': 1}
+
+    # make a GET request and parse the returned JSON
+    # note, no timeouts, error handling or all the other things needed to do this for real
+    print("About to perform the GET request...")
+
+    req = urllib.request.Request('http://exp-api:8000/api/v1/getNewestRequestPk')
+
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    resp = json.loads(resp_json)
+    print(resp)
+    context_dict['newest_pk'] = resp['result']['pk']
+
+    req = urllib.request.Request('http://exp-api:8000/api/v1/getHighestRequestPk')
+
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    resp = json.loads(resp_json)
+    print(resp)
+    context_dict['highest_pk'] = resp['result']['pk']
+
     return render(request, "index.html", context_dict)
 
 def request_detail(request, consumerRequest_pk):
     context_dict = {'title': 'New to AWS', 'description': 'Looking for someone to teach me AWS', 'offered_price': 75.0, 'timestamp': 'March 9, 2018.  7:55', 'availability': 'Mondays and Wednesdays', 'consumer_username': 'bob1', 'consumer_email': 'bob1@gmail.com', 'consumer_phone': '434-958-2913', 'producer_username': None}
-    return render(request, "request_detail.html", context_dict)
+
+    req = urllib.request.Request('http://exp-api:8000/api/v1/requestDetail/' + str(consumerRequest_pk))
+
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    resp = json.loads(resp_json)
+    print(resp)
+
+    if resp['ok']:
+
+        context_dict['title'] = resp['result']['title']
+        context_dict['description'] = resp['result']['description']
+        context_dict['offered_price'] = resp['result']['offered_price']
+        context_dict['timestamp'] = resp['result']['timestamp']
+        context_dict['availability'] = resp['result']['availability']
+        context_dict['consumer_username'] = resp['result']['consumer_username']
+        context_dict['consumer_email'] = resp['result']['consumer_email']
+        context_dict['consumer_phone'] = resp['result']['consumer_phone']
+        context_dict['producer_username'] = resp['result']['producer_username']
+
+        return render(request, "request_detail.html", context_dict)
+
+    else:
+        return HttpResponse("Consumer request does not exist.")
