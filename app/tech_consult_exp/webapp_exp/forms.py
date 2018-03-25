ae417291b513266d730f85c15019406a194ef31a
LiXiaoying <lixiaoying@d-172-25-98-23.dhcp.virginia.edu>
2018-03-24 21:09:21 -0400

    Login and logout functionality

--- app/tech_consult_exp/webapp_exp/forms.py
+++ app/tech_consult_exp/webapp_exp/forms.py
@@ -5,3 +5,7 @@ class LoginForm(forms.Form):
     username = forms.CharField(max_length=200, help_text="Username: ")
     password = forms.CharField(max_length=200, help_text="Password: ")
     is_consumer = forms.BooleanField(required=False)
+
+# Form to delete an authenticator
+class EnterAuthenticatorForm(forms.Form):
+    authenticator = forms.CharField(max_length=256, help_text="Authenticator: ")
